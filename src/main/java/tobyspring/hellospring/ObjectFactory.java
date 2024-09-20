c0a0d08ea5249b565b44c598e03b79568402e0ba
Sumsan38 <ndanl4647@gmail.com>
2024-09-20 23:28:04 +0900

    스프링 컨테이너와 의존관계 주입

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,12 +1,18 @@
 package tobyspring.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration // 빈끼리의 연관관계를 알려준다
 public class ObjectFactory {
 
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
+    @Bean
     public ExRateProvider exRateProvider() {
-        return new WebApiExRateProvider();
+        return new SimpleExRateProvider();
     }
 }
