4d75973ea4c9cd77c747aa6d94e6052206a83229
Sumsan38 <ndanl4647@gmail.com>
2024-09-22 19:35:37 +0900

    싱글톤 레지스트리

--- src/main/java/tobyspring/hellospring/ObjectFactory.java
+++ src/main/java/tobyspring/hellospring/ObjectFactory.java
@@ -1,21 +1,31 @@
 package tobyspring.hellospring;
 
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration // 빈끼리의 연관관계를 알려준다
-@ComponentScan
 public class ObjectFactory {
 
-    /*
     @Bean
     public PaymentService paymentService() {
         return new PaymentService(exRateProvider());
     }
 
     @Bean
+    public OrderService orderService(){
+        return new OrderService(exRateProvider());
+    }
+
+    @Bean
     public ExRateProvider exRateProvider() {
         return new SimpleExRateProvider();
     }
-    */
+
+    class OrderService {
+        ExRateProvider exRateProvider;
+
+        public OrderService(ExRateProvider exRateProvider) {
+            this.exRateProvider = exRateProvider;
+        }
+    }
 }
--- src/main/java/tobyspring/hellospring/PaymentService.java
+++ src/main/java/tobyspring/hellospring/PaymentService.java
@@ -1,15 +1,12 @@
 package tobyspring.hellospring;
 
-import org.springframework.stereotype.Component;
-
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 // 프로그램이 실행되는 시점에 애노테이션 정보를 데이터처럼 읽어올 수 있다. 이를 메타 프로그램이라 한다.
-@Component
 public class PaymentService {
-    private final ExRateProvider exRateProvider;
+    ExRateProvider exRateProvider;
 
     public PaymentService(ExRateProvider exRateProvider) {
         this.exRateProvider = exRateProvider;
