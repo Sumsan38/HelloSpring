5b293a6e6e0c91db9110d4bd4935582853a1d0eb
Sumsan38 <ndanl4647@gmail.com>
2024-09-25 21:53:27 +0900

    테스트와 DI (1)

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -2,34 +2,41 @@ package tobyspring.hellospring.payment;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import tobyspring.hellospring.exrate.WebApiExRateProvider;
+import org.springframework.lang.NonNull;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static java.math.BigDecimal.TEN;
+import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
 class PaymentServiceTest {
 
     @Test
     @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
-    void prepare() throws IOException {
-        // 준비
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+    void convertedAmount() throws IOException {
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1000), valueOf(10_000));
+        testAmount(valueOf(3000), valueOf(30_000));
+
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+
+
+    @NonNull
+    private static void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
 
         // 기능 실행
-        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+        Payment payment = paymentService.prepare(1L, "USD", TEN);
 
         // 검증
         // 1. 환율 정보를 가져온다
-        assertThat(payment.getExRate()).isNotNull();
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
 
         // 2. 원화 환산 금액 계산
-        assertThat(payment.getConvertedAmount())
-                .isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
-
-        // 3. 원화 환산 금액의 유효시간 계산
-        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+        assertThat(payment.getConvertedAmount()).isEqualTo(convertedAmount);
     }
 }
