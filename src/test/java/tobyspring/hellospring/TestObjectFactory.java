9d5c58902cdb952b61f72661c1e3dc4bcc6723bf
Sumsan38 <ndanl4647@gmail.com>
2024-09-25 22:13:15 +0900

    테스트와 DI (2)

---
+++ src/test/java/tobyspring/hellospring/TestObjectFactory.java
@@ -0,0 +1,23 @@
+package tobyspring.hellospring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tobyspring.hellospring.payment.ExRateProvider;
+import tobyspring.hellospring.payment.ExRateProviderStub;
+import tobyspring.hellospring.payment.PaymentService;
+
+import java.math.BigDecimal;
+
+@Configuration // 빈끼리의 연관관계를 알려준다
+public class TestObjectFactory {
+
+    @Bean
+    public PaymentService paymentService() {
+        return new PaymentService(exRateProvider());
+    }
+
+    @Bean
+    public ExRateProvider exRateProvider() {
+        return new ExRateProviderStub(BigDecimal.valueOf(1_000));
+    }
+}
